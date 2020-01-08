2dcdae0d6c8c378f43a9544194653ca89406386b
User <user@Users-MacBook-Pro.local>
2020-01-08 14:37:46 +0000

    app.vue added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <booking-form />
+    <booking-grid :bookings="bookings" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BookingForm from './components/BookingForm';
+import BookingsGrid from './components/BookingsGrid';
+import { eventBus } from './main';
+import BookingService from './services/SBookingService';
 
 export default {
   name: 'app',
+  data () {
+    return {
+      bookings: []
+    }
+  },
   components: {
-    HelloWorld
+    'booking-form': BookingForm,
+    'booking-grid': BookingGrid
+  },
+  mounted(){
+    this.fetchData();
+
+    eventBus.$on('booking-added', booking => this.bookings.push(booking));
+
+    eventBus.$on('booking-deleted', id => {
+      const index = this.bookings.findIndex(booking => booking._id === id);
+      this.bookings.splice(index, 1);
+    })
+  },
+  methods: {
+    fetchData(){
+        BookingService.getBookings()
+        .then(bookings => this.bookings = bookings);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+  height: 100%;
+}
+
+body {
+  background: url('./assets/birds-background.jpg') no-repeat;
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
 }
 </style>
