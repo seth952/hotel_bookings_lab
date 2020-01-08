da1a751446b3c655a0e1476d6b87776d86da4abc
Grant Houston <grant.houston44@hotmail.com>
2020-01-08 13:14:57 +0000

    server.js

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const parser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('@/helpers/create_router.js')
+const cors = require('cors')
+
+app.use(parser.json());
+app.use(cors());
+
+MongoClient.connect('mongodb://localhost:27017')
+.then((client)=>{
+  const db = client.db('hotel')
+  const guestsCollection = db.collection('guests')
+  const guestsRouter = createRouter(guestsCollection);
+  app.use('/api/guests', guestsRouter)
+})
+
+app.listen(3000 function () {
+  console.log(`listening on the year ${this.address().port}`)
+})
